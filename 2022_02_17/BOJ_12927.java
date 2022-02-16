ab1911d8819c6fbebd55369b1c1c258783985ae8
dltmdgh579 <dltmdgh579@gmail.com>
2022-02-16 20:16:44 +0900

    배수 스위치

---
+++ 2022_02_17/BOJ_12927.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class BOJ_12927 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		char[] arr = new char[str.length()];
+		
+		for(int i=0; i<str.length(); i++) {
+			arr[i] = str.charAt(i);
+		}
+		
+		int count = 0;
+		int mul = 0;
+		
+		for(int i=0; i<str.length(); i++) {
+			if(arr[i] == 'N') continue;
+			else {
+				count++;
+				mul = i+1;
+			}
+			
+			for(int j=mul-1; j<str.length(); j+=mul) {
+				arr[j] = arr[j]=='Y'?'N':'Y';
+			}
+			
+			for(int j=0; j<str.length(); j++) {
+				if(arr[j] == 'N') continue;
+				else {
+					break;
+				}
+			}
+		}
+		System.out.println(count);
+	}
+}
