b24da308303204b87dc9ea4bca9c5d409bf3d237
dltmdgh579 <dltmdgh579@gmail.com>
2022-02-16 21:25:34 +0900

    가장 가까운 공통 조상

--- 2022_02_14/BOJ_3584.java
+++ 2022_02_14/BOJ_3584.java
@@ -4,7 +4,6 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_3584{
-    // static ArrayList<ArrayList<Integer>> conn;
     static int[] parentSearch;
     static int A, B, N;
     public static void main(String[] args) throws IOException{
