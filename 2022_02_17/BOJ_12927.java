3233d8aca8d11d34782eb0f931edb0a24f037d0b
dltmdgh579 <dltmdgh579@gmail.com>
2022-02-17 21:39:32 +0900

    배수 스위치

--- 2022_02_17/BOJ_12927.java
+++ 2022_02_17/BOJ_12927.java
@@ -9,6 +9,7 @@ public class BOJ_12927 {
 		
 		char[] arr = new char[str.length()];
 		
+		// 입력
 		for(int i=0; i<str.length(); i++) {
 			arr[i] = str.charAt(i);
 		}
@@ -17,12 +18,14 @@ public class BOJ_12927 {
 		int mul = 0;
 		
 		for(int i=0; i<str.length(); i++) {
+			// 스위치가 켜져 있을 경우만 고려
 			if(arr[i] == 'N') continue;
 			else {
-				count++;
-				mul = i+1;
+				count++; // 스위치 작동 횟수
+				mul = i+1; // 배수
 			}
 			
+			// 첫 스위치가 켜져 있을 경우 모든 배수 반전
 			for(int j=mul-1; j<str.length(); j+=mul) {
 				arr[j] = arr[j]=='Y'?'N':'Y';
 			}
