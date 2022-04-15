dd856313c526fbb68f1bca4de701310fce09e2c2
이승호 <dltmdgh579@gmail.com>
2022-04-16 01:03:35 +0900

    케빈 베이컨의 6단계 법칙

--- BOJ/BOJ_1389.java
+++ BOJ/BOJ_1389.java
@@ -1,74 +1,65 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
- 
+
 public class BOJ_1389 {
-    static final int INF = 987654321;
- 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
- 
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N + 1][N + 1];
- 
-        // 초기값 설정
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
+
+        int[][] arr = new int[N+1][N+1];
+
+        int INF = 10000000;
+
+        // 초기화
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=N; j++){
+                if(i == j) continue;
                 arr[i][j] = INF;
- 
-                if (i == j) {
-                    arr[i][j] = 0;
-                }
             }
         }
         
-        // 간선의 방향이 양방향이어야 함.
-        for (int i = 0; i < M; i++) {
+        // 간선 입력
+        for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
- 
-            arr[x][y] = arr[y][x] = 1;
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = 1;
+            arr[b][a] = 1;
         }
- 
-        // 플로이드 와샬 알고리즘
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
-                    // 최단경로 초기화
-                    if (arr[i][j] > arr[i][k] + arr[k][j]) {
+        
+        // 플로이드 와샬
+        for(int k=1; k<=N; k++){
+            for(int i=1; i<=N; i++){
+                if(i == k) continue;
+                for(int j=1; j<=N; j++){
+                    if(arr[i][j] > arr[i][k] + arr[k][j]){
                         arr[i][j] = arr[i][k] + arr[k][j];
                     }
                 }
             }
         }
- 
-        int res = INF;
-        int idx = -1;
- 
-        // 케빈 베이컨의 수가 가장 작은 인덱스를 탐색
-        for (int i = 1; i <= N; i++) {
-            int total = 0;
-            for (int j = 1; j <= N; j++) {
-                total += arr[i][j];
+        
+        int min = Integer.MAX_VALUE;
+        int minIdx = -1;
+
+        // 가장 작은 케빈 베이컨의 수를 가진 인덱스 탐색
+        for(int i=1; i<=N; i++){
+            int sum = 0;
+            for(int j=1; j<=N; j++){
+                sum += arr[i][j];
             }
- 
-            if (res > total) {
-                res = total;
-                idx = i;
+
+            if(sum < min){
+                min = sum;
+                minIdx = i;
             }
         }
- 
-        bw.write(idx + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(minIdx);
     }
- 
 }
