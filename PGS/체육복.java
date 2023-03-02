8b82d806b3f3d8136f8c9fa779de8f0edea86b4b
이승호 <dltmdgh579@gmail.com>
2023-03-02 14:23:32 +0900

    Lv1 / 탐욕법(Greedy)

---
+++ PGS/체육복.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n;
+        
+        int[] students = new int[n+1];
+
+        Arrays.fill(students, 1);
+        
+        for(int i=0; i<lost.length; i++){
+            students[lost[i]]--;
+        }
+        
+        for(int i=0; i<reserve.length; i++){
+            students[reserve[i]]++;
+        }
+        
+        for(int i=1; i<=n; i++){
+            if(students[i] == 0){
+                if(i-1 > 0 && students[i-1] == 2){
+                    students[i]++;
+                    students[i-1]--;
+                }
+                else if (i+1 <= n && students[i+1] == 2){
+                    students[i]++;
+                    students[i+1]--;
+                }
+                else {
+                    answer--;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
