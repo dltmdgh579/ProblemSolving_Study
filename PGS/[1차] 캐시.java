e58f83c6aff653641f20252850f5dddb5fef504e
이승호 <dltmdgh579@gmail.com>
2023-04-01 23:50:29 +0900

    Lv2 / 2018 KAKAO BLIND RECRUITMENT / 구현

---
+++ PGS/[1차] 캐시.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    
+    static final int CACHE_HIT = 1;
+    static final int CACHE_MISS = 5;
+    
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        if(cacheSize == 0) return 5 * cities.length;
+        
+        LinkedList<String> cache = new LinkedList<>();
+        
+        for(int i=0; i<cities.length; i++){
+            String city = cities[i].toUpperCase();
+            
+            if(cache.remove(city)){
+                cache.addFirst(city);
+                answer += CACHE_HIT;
+            } else {
+                if(cache.size() == cacheSize){
+                    cache.pollLast();
+                }
+                
+                cache.addFirst(city);
+                answer += CACHE_MISS;
+            }
+        }
+        
+        return answer;
+    }
+}
