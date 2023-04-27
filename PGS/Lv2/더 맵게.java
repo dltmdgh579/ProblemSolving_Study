5bf3010cae651d2ddfa9c276cc904a737cc25dbd
dltmdgh579 <dltmdgh579@naver.com>
2023-04-27 14:41:51 +0900

    Lv2 / 힙(Heap)

---
+++ PGS/Lv2/더 맵게.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int score : scoville){
+            pq.add(score);
+        }
+        
+        while(pq.peek() < K){
+            if(pq.size() < 2) return -1;
+            int min1 = pq.poll();
+            int min2 = pq.poll();
+            
+            int mix = min1 + min2 * 2;
+            pq.add(mix);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
