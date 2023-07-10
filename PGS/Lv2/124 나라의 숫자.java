b1556ba99e95ae748bb48875486cc82d0ddd3d20
이승호 <dltmdgh579@gmail.com>
2023-07-10 14:42:06 +0900

    Lv2 / 연습문제

---
+++ PGS/Lv2/124 나라의 숫자.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        String[] digit = {"4", "1", "2"};
+        
+        while(n > 0){
+            int tmp = n%3;
+            n /= 3;
+            if(tmp == 0){
+                n--;
+            }
+            answer.insert(0, digit[tmp]);
+        }
+            
+        return answer.toString();
+    }
+}
